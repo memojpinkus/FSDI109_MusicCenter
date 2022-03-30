9336aaefd519e8e5f7e96c2cf56d6cc355d476a0
Pinkuski <memojpinkus@gmail.com>
2022-03-29 22:01:23 -0700

    Third Assignment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,14 @@ import {Link} from "react-router-dom"
 const Home = () => {
     return(
         <div className="home-page">
-            <h1>MusicCenter</h1>
-            <h4>Everything music</h4>
-
-            <Link to="/catalog" className="btn btn-info">
+            <div class="container">
+                <img src="./imgs/mbv.jpg" alt="" />
+                <Link to="/catalog" className="btn btn-info">
                 Check out our catalog
-            </Link>
+                </Link>
+            </div>
+
+            
         </div>
     );
 };
